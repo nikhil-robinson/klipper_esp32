319ed13cf6f5b0ab0391bc53885c4b73e03e16d5
Nikhil Robinson <nikhil@bibox.in>
2024-04-08 16:16:14 +0530

    folder structure done

---
+++ components/board/include/board/internal.h
@@ -0,0 +1,5 @@
+#include "autoconf.h" // CONFIG_FLASH_APPLICATION_ADDRESS
+#include "stdlib.h"
+
+
+void command_reset(uint32_t *args);
---
+++ components/board/internal.c
@@ -0,0 +1,13 @@
+#include "autoconf.h" // CONFIG_FLASH_APPLICATION_ADDRESS
+#include "board/internal.h" // NVIC_SystemReset
+#include "board/irq.h" // irq_disable
+#include "command.h" // DECL_COMMAND_FLAGS
+#include "esp_system.h"
+
+
+
+void command_reset(uint32_t *args)
+{
+    esp_restart();
+}
+DECL_COMMAND_FLAGS(command_reset, HF_IN_SHUTDOWN, "reset");
