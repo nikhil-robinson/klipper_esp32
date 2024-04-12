f09eac416595991070cc2367bb153ab6d174e492
Nikhil Robinson <nikhil@bibox.in>
2024-04-12 13:11:11 +0530

    builds

--- main/main.c
+++ main/main.c
@@ -5,6 +5,16 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "freertos/task.h"
+
+
+void command_config_reset(uint32_t *args)
+{
+    if (! sched_is_shutdown())
+        shutdown("config_reset only available when shutdown");
+    esp_restart();
+}
+DECL_COMMAND_FLAGS(command_config_reset, HF_IN_SHUTDOWN, "config_reset");
+
 void app_main(void)
 {
     xTaskCreate(sched_main, "sched main", 1024 * 4, NULL, 12, NULL);
